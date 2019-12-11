1501cf99a473de39c4346152983573429c8c573d
yinlixinjava <13269755565@163.com>
2019-12-11 19:48:10 +0800

    事物加上日志

--- changebootparent/changebootchild/src/main/java/com/companyname/userRoleFunction/service/impl/UserServiceImpl.java
+++ changebootparent/changebootchild/src/main/java/com/companyname/userRoleFunction/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.companyname.userRoleFunction.bean.SysUser;
 import com.companyname.userRoleFunction.service.SysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Map;
 
@@ -12,8 +13,9 @@ import java.util.Map;
 public class UserServiceImpl implements SysUserService {
 	@Autowired
 	public MapperDao mapperDao;
-	
-	
+
+
+	@Transactional
 	@Override
 	public void deleteUserAndRoleRelationship(String deteleUser, String deteleRelationship, Map<String, Object> map) {
 		//删除用户表
@@ -26,6 +28,7 @@ public class UserServiceImpl implements SysUserService {
 	/**
 	 * 添加用户和角色之间的关系
 	 */
+	@Transactional
 	@Override
 	public void addUserAndRoleRelationship(String addUser, String addUserAndRoleRelationship, SysUser sysuser, Map<String, Object> map) {
 		//新增一个用户
@@ -35,6 +38,7 @@ public class UserServiceImpl implements SysUserService {
 		mapperDao.insert(addUserAndRoleRelationship, map);
 	}
 
+	@Transactional
 	@Override
 	public void updateUserAndRoleRelationship(String updateUser, String deteleUserAndRoleRelationship,
                                               String insertUserAndRoleRelationship, SysUser sysuser, Map<String, Object> map) {
@@ -48,6 +52,7 @@ public class UserServiceImpl implements SysUserService {
 		
 	}
 
+	@Transactional
 	@Override
 	public int updatepwd(Map<String, String> mapParam) {
 		int result = mapperDao.update("com.companyname.userRoleFunction.bean.SysUserMapper.updatepwd", mapParam);
